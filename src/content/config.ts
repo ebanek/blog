a052599dbc1742383d61ee911a271521184cee4f
ebanek <erik.banek@gmail.com>
2024-08-22 16:17:03 +0200

    Finish first version of about page. Fix metadata and dates for posts.

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,14 +5,9 @@ const postsCollection = defineCollection({
     type: 'content',
     schema: z.object({
       title: z.string(),
-      pubDate: z.date(),
       description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
+      publicationDate: z.date(),
+      lastUpdateDate: z.date(),
     })
 });
 // Export a single `collections` object to register your collection(s)
